7f6d3eab3b62d5fb2b767c30d1ebfc430d8f06e8
Abelas-Solihin <abelassolihin@gmail.com>
2024-10-31 15:47:10 +0700

    Jobsheet8-02 (P.4)

--- RataNilai02.java
+++ RataNilai02.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class RataNilai02 {
     public static void main(String[] args) {
         int i, j;
@@ -8,19 +7,16 @@ public class RataNilai02 {
 
         Scanner sc = new Scanner(System.in);
         
-      
-        for (i = 1; i <= 5; i++) {
+      i=1;
+        while (i <= 5) {
             totalNilai = 0; 
             System.out.println("Input Nilai Mahasiswa ke " + i);
             
-           
             for (j = 1; j <= 5; j++) {
                 System.out.print("Nilai ke-" + j + " = ");
                 nilaiMhs = sc.nextInt();
                 totalNilai += nilaiMhs;
             }
-            
-          
             rataNilai = totalNilai / 5;
             System.out.println("Rata-rata Nilai Mahasiswa ke " + i + " adalah " + rataNilai);
             System.out.println();
